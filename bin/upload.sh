af8db7a1a6eb8716b9f76ed5d63cdea0dd225045
Sanjay Vamja <sanjay@subskribe.com>
2024-09-13 11:31:43 +0530

    ready for publishing

--- bin/upload.sh
+++ bin/upload.sh
@@ -3,13 +3,12 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $SCRIPT_DIR/..
 DIR=$PWD
+source $DIR/.env
 
 log_file=$DIR/tmp/upload.log
-LAMBDA_NAME=sns-to-pushbullet
 
-echo path: $DIR
-echo log_file: $log_file
-echo lambda_name: $LAMBDA_NAME
+echo log path: $log_file
+echo lambda: $AWS_LAMBDA_NAME
 
 echo "Begin" > $log_file
 
@@ -20,7 +19,7 @@ echo "Creating zip file.."
 zip -r ./tmp/function.zip . -x "tmp/*" -x ".git/*" -x ".idea/*" >> $log_file 2>&1
 
 echo "Uploading to AWS.."
-aws --profile svamja lambda update-function-code \
+aws --profile $AWS_PROFILE lambda $AWS_LAMBDA_NAME \
     --function-name $LAMBDA_NAME \
     --zip-file fileb://tmp/function.zip >> $log_file 2>&1 < /dev/null
 
